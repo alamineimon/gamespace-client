dd666daa24ead79d99300013578a023e7d5b5f46
sumanta62 <sumantamajumder62@gmail.com>
2023-01-13 13:06:58 +0600

    bottom Navbar

---
+++ src/Pages/Shared/Navbar/NavbarBottom/NavbarBottom.js
@@ -0,0 +1,203 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { BiSearch } from "react-icons/bi";
+import "./NavbarBottom.css";
+
+const NavbarBottom = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    return (
+        <div>
+            <div className='navbarBottomBG p-3'>
+                <div className='px-4 py-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+                    <div className='relative flex items-center justify-between'>
+
+                        <ul className='items-center hidden space-x-8 lg:flex'>
+                            <li>
+                                <Link
+                                    to='/home'
+                                    className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                >
+                                    Home
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link
+                                    to='/shop'                                  
+                                    className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                >
+                                    Shop
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to='/game'
+                                    className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                >
+                                    Game
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link
+                                    to='/aboute'                                  
+                                    className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                >
+                                   Aboute
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link
+                                    to='/blog'
+                                    className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                >
+                                    Blog
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to='/support'
+                                    className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                >
+                                    Support
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to='/htmlgames'
+                                    className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                >
+                                    HTML Games
+                                </Link>
+                            </li>
+
+
+                        </ul>
+                        <Link
+                            to='/'
+                            className='inline-flex items-center'
+                        >
+                            {/* <img className='w-10' src={logo} alt="" /> */}
+                            <BiSearch className='text-2xl'></BiSearch>
+                        </Link>
+                        <div className='lg:hidden'>
+                            <button
+                                aria-label='Open Menu'
+                              
+                                className='p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50'
+                                onClick={() => setIsMenuOpen(true)}
+                            >
+                                <svg className='w-5 text-gray-100' viewBox='0 0 24 24'>
+                                    <path
+                                        fill='currentColor'
+                                        d='M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z'
+                                    />
+                                    <path
+                                        fill='currentColor'
+                                        d='M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z'
+                                    />
+                                    <path
+                                        fill='currentColor'
+                                        d='M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z'
+                                    />
+                                </svg>
+                            </button>
+                            {isMenuOpen && (
+                                <div className='absolute top-0 left-0 w-full z-10 '>
+                                    <div className='p-5 bg-violet-600 border rounded shadow-sm'>
+                                        <div className='flex items-center justify-between mb-4'>
+                                            <div>
+                                            </div>
+                                            <div>
+                                                <button
+                                                    aria-label='Close Menu'
+                                                  
+                                                    className='p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline'
+                                                    onClick={() => setIsMenuOpen(false)}
+                                                >
+                                                    <svg className='w-5 text-gray-100' viewBox='0 0 24 24'>
+                                                        <path
+                                                            fill='currentColor'
+                                                            d='M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z'
+                                                        />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <nav>
+
+                                            <ul className='space-y-4'>
+                                                <li>
+                                                    <Link
+                                                        to='/home'
+                                                        className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                                    >
+                                                        Home
+                                                    </Link>
+                                                </li>
+
+                                                <li>
+                                                    <Link
+                                                        to='/shop'
+                                                        className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                                    >
+                                                        Shop
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link
+                                                        to='/game'
+                                                        className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                                    >
+                                                        Game
+                                                    </Link>
+                                                </li>
+
+                                                <li>
+                                                    <Link
+                                                        to='/aboute'
+                                                        className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                                    >
+                                                        Aboute
+                                                    </Link>
+                                                </li>
+
+                                                <li>
+                                                    <Link
+                                                        to='/blog'
+                                                        className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                                    >
+                                                        Blog
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link
+                                                        to='/support'                                                      
+                                                        className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                                    >
+                                                        Support
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link
+                                                        to='/htmlgame'                                                      
+                                                        className='font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400'
+                                                    >
+                                                        HTML Game
+                                                    </Link>
+                                                </li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavbarBottom;
