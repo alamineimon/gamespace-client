57b71a9ae3044cddd7a15a561f2b76c770071b0c
Mohammad Rafe <mrrafi5869@gmail.com>
2023-01-17 18:38:15 +0600

    rafi

---
+++ src/Pages/Features/Features.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Features = () => {
+    return (
+        <div>
+            <h1>Fantastic Features</h1>
+            <div>
+                <div>
+                    {/* icon */}
+                    <div>
+                        {/* h1, p */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Features;
