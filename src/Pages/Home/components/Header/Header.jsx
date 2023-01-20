d381ee071552032b91274c252636404c440766ed
hdelower68 <hdelower68@gmail.com>
2023-01-20 20:18:19 +0800

    some error fixed

--- src/Pages/Home/components/Header/Header.jsx
+++ src/Pages/Home/components/Header/Header.jsx
@@ -9,25 +9,25 @@ const Header = () => {
       <video src={bgVideos} autoPlay muted loop className="videoBgs"></video>
       <div className="bgOverlay"></div>
       <div className="textArea">
-        <p className="lg:text-8xl sm:text-4xl lg:mt-12 lg:mb-6 font-bold ml-16">
+        <p className="lg:text-8xl text-4xl lg:mt-12 lg:mb-6 font-bold ml-16">
           All Your Games
           <br />{" "}
-          <span className="lg:text-10xl sm:6xl text-yellow-600">
+          <span className="lg:text-8xl text-4xl text-yellow-600">
             is Here...
           </span>
         </p>
-        <p className="lg:text-2xl sm:text-lg w-1/2 ml-16 mb-12">
+        <p className="lg:text-xl text-lg w-1/2 ml-16 mb-12">
           Arcane gives you the power to create the perfect website for your team
           and community.A trully perfect theme for games !
         </p>
-        <div className="lg:flex sm:block ">
+        <div className="lg:flex block lg:justify-left lg:items-left  ">
           <p>
-            <Link className="bg-yellow-600 ml-16 text-white  text-lg uppercase hover:text-white font-semibold w-[200px] text-center px-8 py-2">
+            <Link className="bg-yellow-600  ml-16 text-white  text-lg uppercase hover:text-white font-semibold w-[350px] text-center px-8 py-2">
               Join Now
             </Link>
           </p>
-          <p className="">
-            <Link className="bg-yellow-600 ml-6 text-white  text-lg uppercase hover:text-white font-semibold w-[250px] text-center px-8 py-2">
+          <p className="lg:mt-0 mt-10 ">
+            <Link className="bg-yellow-600 ml-16 text-white  text-lg uppercase hover:text-white font-semibold w-[350px] text-center px-8 py-2">
               Browse Game
             </Link>
           </p>
