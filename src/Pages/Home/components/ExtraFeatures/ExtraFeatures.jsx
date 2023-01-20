28e0afd7fab3dfcb03d131e3b442d528a4d7c066
eimonalamin <eimonalamin@gmail.com>
2023-01-20 11:30:15 +0600

    eimon

--- src/Pages/Home/components/ExtraFeatures/ExtraFeatures.jsx
+++ src/Pages/Home/components/ExtraFeatures/ExtraFeatures.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./ExtraFeatures.css";
 import { BiRun } from "react-icons/bi";
 import { ImDownload } from "react-icons/im";
+import { Link } from "react-router-dom";
 
 const ExtraFeatures = () => {
   return (
@@ -12,7 +13,7 @@ const ExtraFeatures = () => {
           <h5 className="text-2xl font-semibold font-serif">
             Play Games
           </h5>
-          <p className="">Lorem ipsum dolor sit amet.</p>
+          <p className="">You can play your favorite games here..</p>
           <button className="btn rounded-none btn-outline btn-sm mt-4">Play</button>
         </div>
       </div>
@@ -22,53 +23,13 @@ const ExtraFeatures = () => {
           <h5 className="text-2xl font-semibold font-serif ">
             Download Games
           </h5>
-          <p className="">Lorem ipsum dolor sit amet.</p>
-          <button className="btn rounded-none btn-outline btn-sm mt-4">
+          <p className="">You can browse and download your games here..</p>
+          <Link to='/shop' className="btn rounded-none btn-outline btn-sm mt-4">
             Download
-          </button>
+          </Link>
         </div>
       </div>
     </div>
-    // <div className="pt-32">
-    //   <div className="flex flex-col lg:flex-row w-3/4 mx-auto mb-8">
-    //     <div className="relative md:w-1/2 mb-2 md:mb-0">
-    //       <div className="features-image lg:mr-2">
-    //         <img
-    //           src="https://img.freepik.com/free-photo/world-collapse-doomsday-scene-digital-painting_456031-63.jpg?size=626&ext=jpg&ga=GA1.1.207808977.1658386615&semt=sph"
-    //           alt=""
-    //           className="w-full rounded-xl h-80"
-    //         />
-    //       </div>
-    //       <div className="absolute top-20 w-full text-center">
-    //         <BiRun className="text-5xl w-full mx-auto text-yellow-300"></BiRun>
-    //         <h5 className="text-2xl font-semibold font-serif text-black">
-    //           Play Games
-    //         </h5>
-    //         <p className="text-white">Lorem ipsum dolor sit amet.</p>
-    //         <button className="btn btn-warning btn-sm mt-4">Play</button>
-    //       </div>
-    //     </div>
-    //     <div className="relative md:w-1/2">
-    //       <div className="features-image-two">
-    //         <img
-    //           src="https://img.freepik.com/free-photo/cruel-war-scenes-digital-painting_456031-162.jpg?size=626&ext=jpg&ga=GA1.1.207808977.1658386615&semt=sph"
-    //           alt=""
-    //           className="w-full rounded-xl h-80"
-    //         />
-    //       </div>
-    //       <div className="absolute top-20 w-full text-center">
-    //         <ImDownload className="text-4xl w-full mx-auto text-black"></ImDownload>
-    //         <h5 className="text-2xl font-semibold font-serif text-white">
-    //           Download Games
-    //         </h5>
-    //         <p className="text-black">Lorem ipsum dolor sit amet.</p>
-    //         <button className="btn btn-outline btn-secondary btn-sm mt-4">
-    //           Download
-    //         </button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
