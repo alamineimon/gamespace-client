0d88c90e980a7fdb55af1c44401c887054a930e0
BadgerSIO <saadibneomar@gmail.com>
2023-01-23 12:30:32 +0600

    updated

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../../Pages/Shared/Footer/Footer";
 import Navbar from "../../Pages/Shared/Navbar/Navbar";
-import NavB from "../../Pages/Shared/Navbar/NavB/NavB";
 
 const Main = () => {
   return (
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,6 +1,4 @@
 import React from "react";
-import NavbarBottom from "./NavbarBottom/NavbarBottom";
-import NavbarTop from "./NavbarTop/NavbarTop";
 import "./Nav.css";
 import NavT from "./NavT/NavT";
 import NavB from "./NavB/NavB";
