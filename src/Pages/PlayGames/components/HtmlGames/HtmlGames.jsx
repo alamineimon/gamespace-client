b82e25efab3ff3b0939c2a1ed505e188da2b21c2
BadgerSIO <saadibneomar@gmail.com>
2023-01-27 12:24:40 +0600

    bla bla

--- src/Pages/PlayGames/components/HtmlGames/HtmlGames.jsx
+++ src/Pages/PlayGames/components/HtmlGames/HtmlGames.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import './HtmlGames.css'
+// import './HtmlGames.css'
 import Loader from "../../../Shared/Loader/Loader";
 import HtmlGamesCards from "./HtmlGamesCards";
 
@@ -19,7 +19,11 @@ const HtmlGames = () => {
   return (
     <section className="relative">
       <div className="image">
-            <img src="https://img.freepik.com/premium-photo/white-haired-female-warrior-knight-stands-with-drawn-sword-dragon-against-backdrop-snow-capped-mountains-fantasy-artwork-scene-cgi-animation-3d-rendering_557878-1553.jpg?size=626&ext=jpg&ga=GA1.1.207808977.1658386615&semt=sph" alt="" className="w-full h-4/5" />
+        <img
+          src="https://img.freepik.com/premium-photo/white-haired-female-warrior-knight-stands-with-drawn-sword-dragon-against-backdrop-snow-capped-mountains-fantasy-artwork-scene-cgi-animation-3d-rendering_557878-1553.jpg?size=626&ext=jpg&ga=GA1.1.207808977.1658386615&semt=sph"
+          alt=""
+          className="w-full h-4/5"
+        />
       </div>
       <div className="w-11/12 grid lg:grid-cols-3 gap-5 mx-auto absolute top-64 left-20">
         {(htmlGames || []).map((htmlgame, i) => (
