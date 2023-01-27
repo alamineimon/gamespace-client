1e8fe483d61e27b2847379e1462b4a87e3d9221c
hdelower68 <hdelower68@gmail.com>
2023-01-27 21:08:54 +0800

    candyCrash setup done and error solved

--- src/Pages/2Dgames/CandyCrash/components/ScoreBoard.js
+++ src/Pages/2Dgames/CandyCrash/components/ScoreBoard.js
@@ -1,9 +1,9 @@
 const ScoreBoard = ({ score }) => {
   return (
     <div className="score-board">
-      <h2>{score}</h2>
+      <h2 className="font-bold text-2xl">{score} Scores</h2>
     </div>
   )
 }
 
-export default ScoreBoard
+export default ScoreBoard ;
