785a6a16fc6b41c0fc7c060f57e6569546b864b3
BadgerSIO <saadibneomar@gmail.com>
2023-02-05 07:22:45 +0600

    delete a html game funtion added

--- src/Pages/Dashboard/AllHtmlGames/AllHtmlGames.jsx
+++ src/Pages/Dashboard/AllHtmlGames/AllHtmlGames.jsx
@@ -1,10 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { toast } from "react-hot-toast";
 import Loader from "../../Shared/Loader/Loader";
 import HtmlCards from "./HtmlCards";
 
 const AllHtmlGames = () => {
-  const { data: htmlgames, isLoading } = useQuery({
+  const {
+    data: htmlgames,
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["play-games"],
     queryFn: async () => {
       const res = await fetch(`https://gamespace-server.vercel.app/play-games`);
@@ -12,6 +17,18 @@ const AllHtmlGames = () => {
       return data;
     },
   });
+  const deleteGame = (id) => {
+    fetch(`https://gamespace-server.vercel.app/deleteHtmlGame/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          toast.success("Deleted successfully");
+          refetch();
+        }
+      });
+  };
   if (isLoading) {
     return <Loader />;
   }
@@ -20,7 +37,7 @@ const AllHtmlGames = () => {
       <h1>All games</h1>
       <div className="grid grid-cols-3 md:grid-cols-6 lg:grid-cols-8 gap-5">
         {htmlgames?.map((game, i) => (
-          <HtmlCards key={game._id} game={game} />
+          <HtmlCards key={game._id} game={game} deleteGame={deleteGame} />
         ))}
       </div>
     </section>
