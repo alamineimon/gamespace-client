6ca8fffe21c83785f5e680ae1a7830c434636a6e
BadgerSIO <saadibneomar@gmail.com>
2023-02-10 17:01:55 +0600

    game collection problem fixed

--- src/Pages/Home/components/GameSlider/GameComment.jsx
+++ src/Pages/Home/components/GameSlider/GameComment.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { AuthContext } from "../../../../context/AuthProvider";
