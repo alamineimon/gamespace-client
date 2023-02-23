36899535ee3e7c79fff31f99ab51c642b99ea864
Saad Ibne Omar <86186495+BadgerSIO@users.noreply.github.com>
2023-02-23 20:33:20 +0600

    connect btn rendering done

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,19 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 // our web app's Firebase configuration
 const firebaseConfig = {
- apiKey: "AIzaSyDr89vnpSLi0GzlOxvI-ZCLQ49_hqlMqqY",
- authDomain: process.env.REACT_APP_authDomain,
- projectId: process.env.REACT_APP_projectId,
- storageBucket: process.env.REACT_APP_storageBucket,
- messagingSenderId: process.env.REACT_APP_messagingSenderId,
- appId: process.env.REACT_APP_appId
+  apiKey: "AIzaSyDr89vnpSLi0GzlOxvI-ZCLQ49_hqlMqqY",
+  //   apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 
-export default auth ;
+export default auth;
--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -5,7 +5,7 @@ import { AuthContext } from "../../context/AuthProvider";
 import Footer from "../../Pages/Shared/Footer/Footer";
 import Loader from "../../Pages/Shared/Loader/Loader";
 import Navbar from "../../Pages/Shared/Navbar/Navbar";
-// import MessengerCustomerChat from "react-messenger-customer-chat";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 const Main = () => {
   const { userLoading } = useContext(AuthContext);
@@ -16,10 +16,10 @@ const Main = () => {
     <div>
       <Navbar />
       <Outlet />
-      {/* <MessengerCustomerChat
+      <MessengerCustomerChat
         pageId="113927341610869"
         appId="3256605467984450"
-      /> */}
+      />
       <Footer />
     </div>
   );
